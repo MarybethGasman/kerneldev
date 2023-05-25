24015142595038fce1bf5af67ebf256dfae1f279
MarybethGasman <2934482133@qq.com>
2023-05-25 09:12:23 +0800

    feat: multiple source tree

--- launch.sh
+++ launch.sh
@@ -2,13 +2,13 @@
 # sudo apt-get update
 # sudo apt-get install -y bison flex libelf-dev cpio build-essential libssl-dev qemu-system-x86
 
+# export SRC=rsf-linux/ && ./launch.sh
 
 qemu-system-x86_64 \
-    -kernel linux/arch/x86/boot/bzImage \
+    -kernel $SRC/arch/x86/boot/bzImage \
     -initrd initramfs.img \
     -append "console=ttyS0 root=/dev/ram" \
     -fsdev local,security_model=passthrough,id=fsdev0,path=share \
     -device virtio-9p-pci,id=fs0,fsdev=fsdev0,mount_tag=hostshare \
-    -enable-kvm \
     -m 4G \
 	-nographic 
--- linux-build.sh
+++ linux-build.sh
@@ -1,35 +1,36 @@
 #sudo apt-get -q update
 #sudo apt-get -q install -y bison flex libelf-dev cpio build-essential libssl-dev qemu-system-x86
 
+# export SRC=rsf-linux && ./linux-build.sh
 
 echo "[+] Building kernel..."
-make -C linux defconfig
-echo "CONFIG_NET_9P=y" >> linux/.config
-echo "CONFIG_NET_9P_DEBUG=n" >> linux/.config
-echo "CONFIG_9P_FS=y" >> linux/.config
-echo "CONFIG_9P_FS_POSIX_ACL=y" >> linux/.config
-echo "CONFIG_9P_FS_SECURITY=y" >> linux/.config
-echo "CONFIG_NET_9P_VIRTIO=y" >> linux/.config
-echo "CONFIG_VIRTIO_PCI=y" >> linux/.config
-echo "CONFIG_VIRTIO_BLK=y" >> linux/.config
-echo "CONFIG_VIRTIO_BLK_SCSI=y" >> linux/.config
-echo "CONFIG_VIRTIO_NET=y" >> linux/.config
-echo "CONFIG_VIRTIO_CONSOLE=y" >> linux/.config
-echo "CONFIG_HW_RANDOM_VIRTIO=y" >> linux/.config
-echo "CONFIG_DRM_VIRTIO_GPU=y" >> linux/.config
-echo "CONFIG_VIRTIO_PCI_LEGACY=y" >> linux/.config
-echo "CONFIG_VIRTIO_BALLOON=y" >> linux/.config
-echo "CONFIG_VIRTIO_INPUT=y" >> linux/.config
-echo "CONFIG_CRYPTO_DEV_VIRTIO=y" >> linux/.config
-echo "CONFIG_BALLOON_COMPACTION=y" >> linux/.config
-echo "CONFIG_PCI=y" >> linux/.config
-echo "CONFIG_PCI_HOST_GENERIC=y" >> linux/.config
-echo "CONFIG_GDB_SCRIPTS=y" >> linux/.config
-echo "CONFIG_DEBUG_INFO=y" >> linux/.config
-echo "CONFIG_DEBUG_INFO_REDUCED=n" >> linux/.config
-echo "CONFIG_DEBUG_INFO_SPLIT=n" >> linux/.config
-echo "CONFIG_DEBUG_FS=y" >> linux/.config
-echo "CONFIG_DEBUG_INFO_DWARF4=y" >> linux/.config
-echo "CONFIG_DEBUG_INFO_BTF=y" >> linux/.config
-echo "CONFIG_FRAME_POINTER=y" >> linux/.config
-make -C linux -j6 bzImage
+make -C $SRC defconfig
+echo "CONFIG_NET_9P=y" >> $SRC/.config
+echo "CONFIG_NET_9P_DEBUG=n" >> $SRC/.config
+echo "CONFIG_9P_FS=y" >> $SRC/.config
+echo "CONFIG_9P_FS_POSIX_ACL=y" >> $SRC/.config
+echo "CONFIG_9P_FS_SECURITY=y" >> $SRC/.config
+echo "CONFIG_NET_9P_VIRTIO=y" >> $SRC/.config
+echo "CONFIG_VIRTIO_PCI=y" >> $SRC/.config
+echo "CONFIG_VIRTIO_BLK=y" >> $SRC/.config
+echo "CONFIG_VIRTIO_BLK_SCSI=y" >> $SRC/.config
+echo "CONFIG_VIRTIO_NET=y" >> $SRC/.config
+echo "CONFIG_VIRTIO_CONSOLE=y" >> $SRC/.config
+echo "CONFIG_HW_RANDOM_VIRTIO=y" >> $SRC/.config
+echo "CONFIG_DRM_VIRTIO_GPU=y" >> $SRC/.config
+echo "CONFIG_VIRTIO_PCI_LEGACY=y" >> $SRC/.config
+echo "CONFIG_VIRTIO_BALLOON=y" >> $SRC/.config
+echo "CONFIG_VIRTIO_INPUT=y" >> $SRC/.config
+echo "CONFIG_CRYPTO_DEV_VIRTIO=y" >> $SRC/.config
+echo "CONFIG_BALLOON_COMPACTION=y" >> $SRC/.config
+echo "CONFIG_PCI=y" >> $SRC/.config
+echo "CONFIG_PCI_HOST_GENERIC=y" >> $SRC/.config
+echo "CONFIG_GDB_SCRIPTS=y" >> $SRC/.config
+echo "CONFIG_DEBUG_INFO=y" >> $SRC/.config
+echo "CONFIG_DEBUG_INFO_REDUCED=n" >> $SRC/.config
+echo "CONFIG_DEBUG_INFO_SPLIT=n" >> $SRC/.config
+echo "CONFIG_DEBUG_FS=y" >> $SRC/.config
+echo "CONFIG_DEBUG_INFO_DWARF4=y" >> $SRC/.config
+echo "CONFIG_DEBUG_INFO_BTF=y" >> $SRC/.config
+echo "CONFIG_FRAME_POINTER=y" >> $SRC/.config
+make -C $SRC -j6 bzImage
